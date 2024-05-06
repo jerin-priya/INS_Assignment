74f6d2e0e453eb63a577e822cb55f9a35e630641
Jerin Hasan Priya <priyajerin9@gmail.com>
2024-05-07 01:09:24 +0600

    done

---
+++ Lab_Task1/Sub_Task1/task.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string ciphertext = "odroboewscdrolocdcwkbdmyxdbkmdzvkdpybwyeddrobo";
+
+    for (int shift = 1; shift < 26; ++shift) {
+        string plaintext = "";
+        for (char c : ciphertext) {
+            if (isalpha(c)) {
+                char shifted = c - shift;
+                if (islower(c)) {
+                    if (shifted < 'a') {
+                        shifted += 26;
+                    }
+                } else if (isupper(c)) {
+                    if (shifted < 'A') {
+                        shifted += 26;
+                    }
+                }
+                plaintext += shifted;
+            } else {
+                plaintext += c;
+            }
+        }
+        cout << "Shift " << shift << ": " << plaintext << endl;
+    }
+
+    return 0;
+}
